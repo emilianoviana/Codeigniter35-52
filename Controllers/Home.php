0b122394213afe9da1397b6e013c272f2d03ed99
emilianoviana <87295554+emilianoviana@users.noreply.github.com>
2021-11-27 14:00:53 +0700

    PHP

---
+++ Controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        
+            echo "<center>";
+            echo "<h1>WELCOME TO RESTORAN CEMIL4N</h1>";
+            echo "</center>";
+        
+    }
+}
---
+++ Controllers/Login.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Login extends BaseController
+{
+    public function index()
+    {
+        
+        echo "<h1>WELCOME TO RESTORAN CEMIL4N</h1>";
+    
+    }
+}
